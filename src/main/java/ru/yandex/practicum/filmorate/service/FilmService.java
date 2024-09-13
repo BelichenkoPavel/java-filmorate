233fdfec302f33be0eb113088ffc5c42fa746267
Pavel <ppbelichenko@yandex.ru>
2024-09-13 17:36:10 +0300

    убрал неиспользуемый ArrayList

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -15,7 +15,6 @@ import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 

b84c3271767efaf26b169e12070ef740fc1433ee
Pavel <ppbelichenko@yandex.ru>
2024-10-12 18:00:54 +0300

    fix lint

--- src/test/java/ru/yandex/practicum/filmorate/storage/db/DBFilmStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/db/DBFilmStorageTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.MPA;
@@ -17,7 +16,6 @@ import java.util.HashSet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
